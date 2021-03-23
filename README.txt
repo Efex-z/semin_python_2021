42c68a3a0f53622f3016f73ce389b270e6139eb8
Fedex <fedexmorlan@hotmail.com>
2021-03-23 10:45:14 -0300

    agregar readme principal y modificaciones ej2 (practico1)

---
+++ README.txt
@@ -0,0 +1,4 @@
+Nombre y apellido: Federico Martín Morlan
+número de alumno: 17842/7
+
+Las carpeta "practico1" contiene la resolucion de los desafíos de la clase 1 
--- practico1/ej2.py
+++ practico1/ej2.py
@@ -1,28 +1,28 @@
-#----------- primer desafío ----------------
-#lee un numero y determina si es par o impar
+# ----------- primer desafío ----------------
+# lee un numero y determina si es par o impar
 num = int(input("ingresa un número: "))
-if (num%2 == 0):
+if (num % 2 == 0):
     print("es par")
 else:
     print("NO es par")
 
-#--------------segundo desafío ------------------------
-#lee un numero de teclado e imprime si es multiplo de 2, 3 o 5.
-#terminé no utilizando elif porque puede ser multiplo de 2 y 5 ó 3 y 5.
+# --------------segundo desafío ------------------------
+# lee un numero de teclado e imprime si es multiplo de 2, 3 o 5.
+# terminé no utilizando elif porque puede ser multiplo de 2 y 5 ó 3 y 5.
 num = int(input("ingresa un número más: "))
-if (num%2 == 0):
+if (num % 2 == 0):
     print("es múltiplo de 2. ")
-#puede ser multiplo de 2 y de 5.
-if (num%5 == 0):
+# puede ser multiplo de 2 y de 5.
+if (num % 5 == 0):
     print("es múltiplo de 5. ")
-#puede ser multiplo de 3 y de 5.
-if (num%3 == 0):
+# puede ser multiplo de 3 y de 5.
+if (num % 3 == 0):
     print("es múltiplo de 3. ")
 else:
-    print("no es múltiplo ni de 2, ni de 3 ni de 5.") 
+    print("no es múltiplo ni de 2, ni de 3 ni de 5.")
 
-#--------------tercer desafío -------------------------
-#dado una letra ingresada imprime si es mayuscula o minuscula
+# --------------tercer desafío -------------------------
+# dado una letra ingresada imprime si es mayuscula o minuscula
 char = input("Ingresar una letra: ")
 if (char >= "A") and (char <= "Z"):
     print("Es mayúscula")
@@ -31,17 +31,17 @@ elif (char >= "a") and (char <= "z"):
 else:
     print("No es una letra!")
 
-#-------------cuarto desafío --------------------------
-#imprime si un caracter ingresado es una comilla o no.
+# -------------cuarto desafío --------------------------
+# imprime si un caracter ingresado es una comilla o no.
 char = input("Ingresar un caracter: ")
-#envuelvo el caracter de comilla con comillas diferentes (como alternativa a escapatoria con barra invertida)
+# envuelvo el caracter de comilla con comillas diferentes (como alternativa a escapatoria con barra invertida)
 if (char == "'") or (char == '"'):
     print("es una comilla.")
 else:
     print("no es una comilla.")
 
-#-------------quinto desafío ----------------------------
-#dadas dos cadenas ingresadas por teclado, imprime la mas larga
+# -------------quinto desafío ----------------------------
+# dadas dos cadenas ingresadas por teclado, imprime la mas larga
 str1 = input("Ingresar una cadena de texto: ")
 str2 = input("Ingresar otra cadena de texto: ")
 if (len(str1) > len(str2)):
@@ -51,13 +51,12 @@ elif (len(str1) < len(str2)):
 else:
     print("las dos cadenas ingresadas tienen igual longitud.")
 
-
-#-------------sexto desafio -------------------------------
-#imprime cuantas letras "a" contiene una cadena de caracteres
+# -------------sexto desafio -------------------------------
+# imprime cuantas letras "a" contiene una cadena de caracteres
 str1 = input("Ingresar una nueva cadena de texto: ")
 total_a = 0
 for c in str1:
     if c == "a":
         total_a = total_a + 1
 
-print ("la cadena tiene "+ str(total_a) +" letras 'a'.")
+print("la cadena tiene " + str(total_a) + " letras 'a'.")
