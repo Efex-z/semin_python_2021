292f5253157fb778add36b868d6cfe90dd3f219a
Federico Morlan <69846740+fedeMorlan@users.noreply.github.com>
2021-04-20 15:36:42 -0300

    Agregue la consigna 2
    habia hecho la consigna 1 antes de las 11 pero la 2 no llegue a pensarla de manera correcta en ese momento y la hice luego.

--- desafiosTeoria/teoria6_20-4.py
+++ desafiosTeoria/teoria6_20-4.py
@@ -6,24 +6,27 @@ archivo = open('appstore_games.csv', "r", encoding='utf-8')
 csvreader = csv.reader(archivo, delimiter=',')
 
 print(next(csvreader)) # solo para saber el formato:
+
 # ['URL', 'ID', 'Name', 'Subtitle', 'Icon URL', 'Average User Rating', 'User Rating Count', 'Price',
 # 'In-app Purchases', 'Description', 'Developer', 'Age Rating', 'Languages', 'Size', 'Primary Genre', 'Genres',
 # 'Original Release Date', 'Current Version Release Date']
 
 lista_gratuitos_espanol = filter(lambda x: (x[7] == '0') and 'ES' in x[12], csvreader)
 print('Juegos gratuitos en español: ')
-#for elemento in lista_gratuitos_espanol:
-#    print(elemento)
-# nota: lo deje comentado para no imprimir todo (lleva tiempo porque son muchos) y poder testear la segunda consigna
+for elemento in lista_gratuitos_espanol:
+   print(elemento)
 print()
 # fin de desafio 1 -----------------------------------------------------------------------------------------------
+# nota: lo que está arriba lo agregue antes de las 11. lo que sigue a continuacion lo tuve que rehacer:
 
 contador = collections.Counter()
-mejorpuntaje = []
+juegos = {} # vamos a hacer un diccionario que contenga como clave los juegos y como valor el puntaje
+
 for linea in csvreader:
-    contador.update(linea)
+    if linea[6] != '':
+        juegos.update({linea[2]: int(linea[6])})
+contador.update(juegos)
+# pongo en el contador todos los datos del diccionario (por esto los castee a int, para poder ordenarlos por ocurrencia)
+# imprimiendo entonces los 10 primeros del contador, automaticamente ya tengo lo solicitado.
+print (contador.most_common(10))
 
-# no llego a pensar como filtrar primero los elementos que tengan mas user rating count
-
-
-print(contador.most_common(10))
